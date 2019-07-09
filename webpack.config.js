8bf22b4af8d2cd7cd774a06141d4035f83ac80fd
jaehaCodeit <jaeha@codeit.kr>
2019-07-09 10:11:50 +0900

    Setup devDependencies and dist fold

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: { // loader - customize behavior of web pack when it loads a given file
@@ -43,7 +43,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map', // sourcemap
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   }
 }
